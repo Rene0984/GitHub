24329e036d1bfbcfef309341ba79600ebbfcf467
Rene0984 <106356604+Rene0984@users.noreply.github.com>
2024-10-10 20:31:46 -0600

    img 1

--- practicas/tailwind.config.js
+++ practicas/tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
         sans: ['New Super Mario Font U', 'sans-serif']
       },
       backgroundImage: theme => ({
-        'wave-pattern': "url('.public/img/wave-white.png')",
-        'wave-pattern-dots': "url('../img/pattern-white-dots.png')",
-        'pattern-characters-red': "url('../img/pattern-characters-red.png')",
-        'character-mario': "url('../img/character-l.png')",
-        'wave-pink': "url('../img/wave-pink.png')",
-        'yellow-dots': "url('../img/pattern-yellow-dots.png')",
+        'wave-pattern': "url('./public/img/wave-white.png')",
+        'wave-pattern-dots': "url('./public/img/pattern-white-dots.png')",
+        'pattern-characters-red': "url('./public/img/pattern-characters-red.png')",
+        'character-mario': "url('./public/img/character-l.png')",
+        'wave-pink': "url('./public/img/wave-pink.png')",
+        'yellow-dots': "url('./public/img/pattern-yellow-dots.png')",
       }),
       screens:{
         'xsm': '320px'
